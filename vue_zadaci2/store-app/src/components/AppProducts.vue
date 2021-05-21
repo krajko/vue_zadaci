a9bd9aa99608f94e83b255359a413e137dcc1fc5
krajko <nikolakrajinovic99@gmail.com>
2021-05-21 15:58:09 +0200

    zadatak 7

--- vue_zadaci2/store-app/src/components/AppProducts.vue
+++ vue_zadaci2/store-app/src/components/AppProducts.vue
@@ -2,17 +2,21 @@
     <div class="container">
         <input v-model="query" class="form-control w-25 mt-5" placeholder="Search products">
         <ul class="list-unstyled mt-5">
-            <li v-for="product in filteredProducts" :key="product.id" class="my-4">
+            <li v-for="(product, i) in filteredProducts" :key="product.id" class="my-4">
 
                 <div class="d-flex flex-row mx-auto" style="width: 30vw">
                     <div class="col-6 text-start">
                         <p class="my-0 py-0"> {{ product.title }} </p>
                     </div>
                     <div class="col d-flex justify-content-end align-items-end text-end">
-                        <p class="my-0 py-0" style="font-size: .9rem"> Quantity: {{ product.quantity }} </p>
+                        <p class="mx-1 my-0 py-0" style="font-size: .9rem"> Quantity: </p>
+                        <button @click="inc(i)" class="btn">+</button>
+                        <p class="mx-1 my-0 py-0" style="font-size: .9rem"> {{ product.quantity }}</p>
+                        <button @click="dec(i)" class="btn">-</button>
                     </div>
                 </div>
                 <hr class="mx-auto my-0 py-0" style="width: 30vw">
+                
             </li>
         </ul>
     </div>
@@ -60,6 +64,16 @@ export default {
                 return product.title.toLowerCase().includes(this.query);
             });
         }
+    },
+    methods: {
+        inc(i) { 
+            this.products[i].quantity++;
+        },  
+        dec(i) {
+            if (this.products[i].quantity > 0) {
+                this.products[i].quantity--;
+            }
+        }
     }
 }
 </script>
@@ -73,5 +87,13 @@ export default {
         border-color: #42b983;
         box-shadow: none;
         box-shadow: 0 1 .1rem #42b983;
+    }
+    .btn {
+        color: #42b983;
+        padding: 0;
+        margin: 0 2px;
+    }
+    .btn:focus {
+        box-shadow: none;
     }
 </style>
