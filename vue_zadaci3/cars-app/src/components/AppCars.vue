7108d65c7405ad8a3e947ce62932145840f9c049
krajko <nikolakrajinovic99@gmail.com>
2021-05-24 21:05:38 +0200

    zadatak 2

--- vue_zadaci3/cars-app/src/components/AppCars.vue
+++ vue_zadaci3/cars-app/src/components/AppCars.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
     <h1>Cars component</h1>
+
+    <ul class="list-unstyled">
+      <li v-for="car in cars" :key="car.id"> {{ car.brand }} {{ car.model }} {{ car.year }} </li>
+      <li v-if="!car">Car list is empty.</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import Cars from '../services/Cars';
+
 export default {
-  name: 'AppCars'
+  name: 'AppCars',
+
+  data() {
+    return {
+      cars: [],
+    }
+  },
+
+  created() {
+    this.cars = Cars.getAll().then(console.log);
+    console.log(this.cars);
+  }
 }
 </script>
 
