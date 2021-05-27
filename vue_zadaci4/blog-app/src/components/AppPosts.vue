54bbc6ad1424535e20a21172dc736d0cd81240c2
krajko <nikolakrajinovic99@gmail.com>
2021-05-27 20:32:02 +0200

    zadatak 2

--- vue_zadaci4/blog-app/src/components/AppPosts.vue
+++ vue_zadaci4/blog-app/src/components/AppPosts.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <h1 class="text-primary">Posts</h1>
+  <div class="container">
+    <ul class="list-unstyled d-flex flex-column justify-content-center">
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+
+      <li v-if="posts.length === 0">No posts to show.</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Posts from '../services/Posts.js';
+
 export default {
-  name: 'AppPosts'
+  name: 'AppPosts',
+
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+    async getPosts () {
+      try {
+        this.posts = await Posts.getAll();
+      } catch(err) {
+        console.log(err);
+        this.posts = [];
+      }
+    }
+  }
 }
 </script>
 
