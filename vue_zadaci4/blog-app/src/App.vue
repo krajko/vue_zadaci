99e2b48892a663f8df52c2768c5f58dd7fc6e04f
krajko <nikolakrajinovic99@gmail.com>
2021-05-27 20:40:13 +0200

    zadatak 2

--- vue_zadaci4/blog-app/src/App.vue
+++ vue_zadaci4/blog-app/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/posts">Posts</router-link>
     </div>
     <router-view/>
   </div>
@@ -30,9 +29,10 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#nav a.router-link-active {
+  color: var(--bs-primary);
 }
 </style>
