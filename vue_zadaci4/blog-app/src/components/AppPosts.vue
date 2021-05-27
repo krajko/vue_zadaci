33fae494144fda79cc670177f5283a4ad700084c
krajko <nikolakrajinovic99@gmail.com>
2021-05-27 20:13:44 +0200

    zadatak 1 + npm paketi

---
+++ vue_zadaci4/blog-app/src/components/AppPosts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-primary">Posts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPosts'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
